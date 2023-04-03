7f43c55c3ad42f153175e0e0465e0570efe34356
JhoneiroDev <jhoneiro12@hotmail.com>
2023-04-02 21:25:57 -0500

    pharmacy leg

---
+++ src/main/main.java
@@ -0,0 +1,15 @@
+package main;
+
+import views.LoginView;
+
+/**
+ *
+ * @author jhone
+ */
+public class main {
+        public static void main(String[] args) {
+        //Instancia del login
+        LoginView login  = new LoginView();
+        login.setVisible(true);
+    }
+}
